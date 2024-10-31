4eb46d1c828a577a4c304898d8f9e0768d0dca08
godwininyene <godwinhigh2@gmail.com>
2024-10-31 18:19:07 +0100

    Fix navigation import issue

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react'
-import Navigation from '../components/navigation';
+import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 import AOS from "aos";
 import 'aos/dist/aos.css';
